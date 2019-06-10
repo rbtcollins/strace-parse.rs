5a2b61dcc3ecd0565ff352184acb38a9cf976aa8
Robert Collins <robertc@robertcollins.net>
2019-06-10 12:45:34 +1200

    Some initial code
    Enough shape to start iterating on. Nothing works. Prepare to be
    disappointed.

---
+++ README.md
@@ -0,0 +1,75 @@
+# Parser for strace files
+
+NB: The recommended strace options include : -ttt -T - -ttt gives microsecond
+absolute timestamps, and -T gives syscall durations. Other options have various
+(poor) tradeoffs.
+
+# API
+
+Parsing layers(core feature):
+* Raw parsing layer: bytes -> structs/enums + day rollover detection.
+* start/stop combination: combine half open events for the same syscall into one
+  closed event
+
+Exporters:
+* Event Trace format(planned)
+
+Analysis layers(speculative):
+* lifetime analysis: analyse lifetime usage of resources
+* concurrency analysis: look for interactions between syscalls
+
+Ideally, no configuration needed, just feed the engine strace content, iterate
+over the desired layer and done. However, if the format proves too ambiguous,
+then the raw parsing layer will be the place that configuration takes place.
+While it is possible to guarantee an unambiguous format by constraining the
+versions and strace options supported, the closer to Just Works the better for
+users - so there is a trade off between ease of use and reliability; for now
+this is biasing to ease of use.
+
+# Contributing
+
+## Parsing failures
+
+At minimum: a bug report with an attached strace snippet demonstrating the
+failure.
+
+If you have time, a PR with a regression test and bug fix would be great.
+
+## Other things
+
+A bug report that describes what you want to achieve, or a PR implementing it -
+with at least enough testing that other authors don't need to worry about
+undoing your work by mistake in future.
+
+## Implementation notes
+
+The parser is an internal detail - it may be reimplemented/switched out/whatever
+in future.
+
+# FAQ
+
+## Q: Dealing with time
+
+Timestamps are optional, or may be relative. So ideally we have absolute microsecond
+timestamps (-ttt), but we may have only same-day (-t/-tt) or relative timestamps (-r)
+or no time at all. In the
+latter case while we can partially order the syscalls we cannot establish a
+timeline, but we can still determine (some) concurrent syscalls using the ordering
+given by `<unfinished ...>` markers.
+For -t/-tt each time the clock rolls over we add an artificial day to the
+durations returned.
+
+
+## Q: Why isn't this just enhancements to
+     https://github.com/wookietreiber/strace-analyzer ?
+  A: https://github.com/wookietreiber/strace-analyzer#features-that-will-not-be-implemented
+     - That project has admirable focus on being a CLI; I want a library to use
+     for various process-a-strace file problems I run into.
+
+# License
+
+Apache-2.0
+
+# Authors
+
+Robert Collins
---
+++ src/lib.rs
@@ -0,0 +1,258 @@
+// Copyright (c) 2019 Robert Collins. Licensed under the Apache-2.0 license.
+
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate nom;
+
+#[macro_use]
+extern crate error_chain;
+
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain! {}
+}
+
+pub mod raw {
+    use crate::errors::*;
+    use std::io::{BufRead, BufReader, Read};
+    use std::time::Duration;
+
+    /// A generic unmodelled syscall:
+    /// any name, with any args of any type and any result of any type
+    #[derive(Clone, Hash, Eq, Debug, PartialEq)]
+    pub struct GenericCall {
+        pub call: String,
+        /// Not currently modelled, but per-call-type
+        pub args: Vec<String>,
+        /// Not currently modelled, but per-call-type
+        pub result: String,
+    }
+
+    /// The syscall or action that happened.
+    #[derive(Clone, Hash, Eq, Debug, PartialEq)]
+    pub enum Call {
+        /// A generic unmodelled syscall was made
+        Generic(GenericCall),
+        /// The process exited
+        Exited(u32),
+    }
+
+    #[derive(Clone, Hash, Eq, Debug, PartialEq)]
+    pub struct Syscall {
+        pub pid: u32,
+        pub call: Call,
+        /// When the system call started, if known.
+        pub start: Option<Duration>,
+        /// When the system call finished, if known.
+        pub stop: Option<Duration>,
+        /// Duration of the system call, if known. Note that duration may be known
+        /// without knowing the start and stop as in the following example:
+        /// ```2 execve("foo", ["bar"], 0x7fffedc2f180 /* 20 vars */) = 0 <0.005029>```
+        pub duration: Option<Duration>,
+    }
+
+    pub type Line = Result<Syscall>;
+
+    struct ParseLines<T: BufRead> {
+        lines: T,
+    }
+
+    impl<T: BufRead> Iterator for ParseLines<T> {
+        type Item = Line;
+
+        fn next(&mut self) -> Option<Line> {
+            let mut line = String::new();
+            // TODO: switch to read_until to deal with non-UTF8 strings in the strace.
+            let len = self.lines.read_line(&mut line);
+            let line = line.as_bytes();
+            named!(t, do_parse!(a: take!(10) >> (a)));
+            match t(&line) {
+                _ => None,
+            }
+        }
+    }
+
+    pub fn parse<T: Read>(source: T) -> impl Iterator<Item = Line> {
+        ParseLines {
+            lines: BufReader::new(source),
+        }
+    }
+}
+
+mod structure {}
+
+#[cfg(test)]
+mod tests {
+    use crate::raw::{Call, GenericCall, Syscall};
+    use std::time::Duration;
+
+    #[test]
+    fn durationonly() {
+        let strace_content = r#"15860 execve("./target/debug/rustup", ["./target/debug/rustup", "uninstall", "nightly"], 0x7fffedc2f180 /* 20 vars */) = 0 <0.005029>
+15860 brk(NULL)                         = 0x7fffc415f000 <0.000034>
+15860 exit_group(0)                     = ?
+15860 +++ exited with 0 +++
+"#.as_bytes();
+        let parsed: Vec<super::raw::Line> = super::raw::parse(strace_content).collect();
+        let expected: Vec<super::raw::Line> = vec![
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Generic(GenericCall {
+                    call: "execve".into(),
+                    args: vec![
+                        r#""./target/debug/rustup""#.into(),
+                        r#"["./target/debug/rustup", "uninstall", "nightly"]"#.into(),
+                        "0x7fffedc2f180 /* 20 vars */".into(),
+                    ],
+                    result: "0".into(),
+                }),
+                start: None,
+                stop: None,
+                duration: Some(Duration::from_micros(5029)),
+            }),
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Generic(GenericCall {
+                    call: "brk".into(),
+                    args: vec!["NULL".into()],
+                    result: "0x7fffc415f000".into(),
+                }),
+                start: None,
+                stop: None,
+                duration: Some(Duration::from_micros(34)),
+            }),
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Generic(GenericCall {
+                    call: "exit_group".into(),
+                    args: vec!["0".into()],
+                    result: "?".into(),
+                }),
+                start: None,
+                stop: None,
+                duration: None,
+            }),
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Exited(0),
+                start: None,
+                stop: None,
+                duration: None,
+            }),
+        ];
+        assert_eq!(parsed.len(), expected.len());
+        for (l, r) in parsed.into_iter().zip(expected.into_iter()) {
+            assert_eq!(l.unwrap(), r.unwrap());
+        }
+    }
+
+    #[test]
+    fn datemicroandduration() {
+        let strace_content = r#"15873 20:03:49.395909 execve("./target/debug/rustup", ["./target/debug/rustup", "install", "nightly"], 0x7ffff2435d98 /* 19 vars */) = 0 <0.008768>
+15873 20:03:49.406540 brk(NULL)         = 0x7ffff644d000 <0.000058>
+16135 20:12:50.069441 exit_group(0)     = ?
+16135 20:12:50.070014 +++ exited with 0 +++
+"#.as_bytes();
+        let parsed: Vec<super::raw::Line> = super::raw::parse(strace_content).collect();
+        let expected: Vec<super::raw::Line> = vec![
+            Ok(Syscall {
+                pid: 15873,
+                call: Call::Generic(GenericCall {
+                    call: "execve".into(),
+                    args: vec![
+                        r#""./target/debug/rustup""#.into(),
+                        r#"["./target/debug/rustup", "install", "nightly"]"#.into(),
+                        "0x7ffff2435d98 /* 19 vars */".into(),
+                    ],
+                    result: "0".into(),
+                }),
+                start: Some(Duration::from_micros(
+                    49_395909 + (((20 * 60) + 3) * 60 * 1_000000),
+                )),
+                stop: Some(Duration::from_micros(
+                    49_395909 + (((20 * 60) + 3) * 60 * 1_000000) + 8768,
+                )),
+                duration: Some(Duration::from_micros(8768)),
+            }),
+            Ok(Syscall {
+                pid: 15873,
+                call: Call::Generic(GenericCall {
+                    call: "brk".into(),
+                    args: vec!["NULL".into()],
+                    result: "0x7ffff644d000".into(),
+                }),
+                start: Some(Duration::from_micros(
+                    49_406540 + (((20 * 60) + 3) * 60 * 1_000000),
+                )),
+                stop: Some(Duration::from_micros(
+                    49_406540 + (((20 * 60) + 3) * 60 * 1_000000) + 58,
+                )),
+                duration: Some(Duration::from_micros(58)),
+            }),
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Generic(GenericCall {
+                    call: "exit_group".into(),
+                    args: vec!["0".into()],
+                    result: "?".into(),
+                }),
+                start: Some(Duration::from_micros(
+                    50_069441 + (((20 * 60) + 12) * 60 * 1_000000),
+                )),
+                stop: None,
+                duration: None,
+            }),
+            Ok(Syscall {
+                pid: 15860,
+                call: Call::Exited(0),
+                start: Some(Duration::from_micros(
+                    50_070014 + (((20 * 60) + 12) * 60 * 1_000000),
+                )),
+                stop: None,
+                duration: None,
+            }),
+        ];
+        assert_eq!(parsed.len(), expected.len());
+        for (l, r) in parsed.into_iter().zip(expected.into_iter()) {
+            assert_eq!(l.unwrap(), r.unwrap());
+        }
+    }
+
+    #[test]
+    fn datemicrorollover() {
+        let strace_content = r#"1 23:59:59.000000 exit_group(0)     = ?
+2 0:0:0.000001 +++ exited with 0 +++
+"#
+        .as_bytes();
+        let parsed: Vec<super::raw::Line> = super::raw::parse(strace_content).collect();
+        let expected: Vec<super::raw::Line> = vec![
+            Ok(Syscall {
+                pid: 1,
+                call: Call::Generic(GenericCall {
+                    call: "exit_group".into(),
+                    args: vec!["0".into()],
+                    result: "?".into(),
+                }),
+                start: Some(Duration::from_micros(
+                    59_000000 + (((23 * 60) + 59) * 60 * 1_000000)
+                )),
+                stop: None,
+                duration: None,
+            }),
+            Ok(Syscall {
+                pid: 2,
+                call: Call::Exited(0),
+                start: Some(Duration::from_micros(
+                    00_000001 + (((24 * 60) + 0) * 60 * 1_000000)
+                )),
+                stop: None,
+                duration: None,
+            }),
+        ];
+        assert_eq!(parsed.len(), expected.len());
+        for (l, r) in parsed.into_iter().zip(expected.into_iter()) {
+            assert_eq!(l.unwrap(), r.unwrap());
+        }
+    }
+}
